39d62b446cc431f7c7c3ca17e3ba09304433b540
lucasantos-S <lucassantos.dsilv@gmail.com>
2021-06-21 19:42:46 -0300

    enviado ao respositorio

---
+++ README.md.txt
@@ -0,0 +1,10 @@
+# :computer: Recriando a interface do Netflix
+
+## ##Neste projeto do bootcamp da DIGITAL INNOVATION ONE de HTML, CSS e JS, recriamos a home page da netflix.
+
+
+
+
+
+
+
---
+++ js/owl/setup.js
@@ -0,0 +1,36 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+    
+})
+
+$('.owl-carousel1').owlCarousel1({
+    loop:true,
+    margin:5,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:10
+        }
+    }
+    
+})
+
